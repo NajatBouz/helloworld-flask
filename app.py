5286d9786632c1f26854002521f5b641998210fc
OlgaTkach1989 <olga.tkachuk378@gmail.com>
2025-05-10 21:06:20 +0200

    Neue Routen hinzugefügt: /hello, /api, /25-01, /about, /info

--- app.py
+++ app.py
@@ -23,5 +23,16 @@ def kurs():
     return "<p>Hallo Kurs 25-01</p>"
 
 
+
+@app.route('/about')
+def about():
+    return "Das ist eine info-Seite über den Kurs 25-01"
+
+
+@app.route('/info')
+def info():
+    return "Das ist eine Info-Seite über Hunde-Salons."
+
+
 if __name__ == "__main__":
     app.run(debug=True)
